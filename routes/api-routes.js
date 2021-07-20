6afb26c0326de29090801aeba20a230ea50684e0
JuneXll <71652307+JuneXll@users.noreply.github.com>
2021-07-20 15:04:39 -0400

    Updated routes for post and get

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,1 +1,35 @@
 const router = require("express").Router();
+const Workout = require("../models/exercise");
+
+//Requests for all routes
+router.get("api/workouts",(req,res)=>{
+    Workout.find()
+        .then(dbData=>{
+            res.json(dbData);
+        })
+        .catch(err=>{
+            res.status(400).json(err);
+        });
+})
+
+//Request for range
+router.get("api/workouts/range",(req,res)=>{
+    Workout.find()
+        .then(dbData=>{
+            res.json(dbData);
+        })
+        .catch(err=>{
+            res.status(400).json(err);
+        })
+})
+
+//Post a workout
+router.post("api/workouts",({body},res)=>{
+    Workout.create(body)
+        .then(dbData=>{
+            res.json(dbData);
+        })
+        .catch(err=>{
+            res.status(404).json(err);
+        })
+})
