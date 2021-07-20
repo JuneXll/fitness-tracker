57e17f4575ec79249f66463ae14e2d2a881bd1b7
JuneXll <71652307+JuneXll@users.noreply.github.com>
2021-07-20 15:18:12 -0400

    Completed api routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,34 +2,53 @@ const router = require("express").Router();
 const Workout = require("../models/exercise");
 
 //Requests for all routes
-router.get("api/workouts",(req,res)=>{
-    Workout.find()
-        .then(dbData=>{
+router.get("api/workouts", async (req,res)=>{
+    try {
+        Workout.find()
+        .then((dbData)=>{
             res.json(dbData);
         })
-        .catch(err=>{
-            res.status(400).json(err);
-        });
+    } catch(err) {
+        res.status(400).json(err);
+        console.log(err);
+    }
 })
 
 //Request for range
-router.get("api/workouts/range",(req,res)=>{
-    Workout.find()
-        .then(dbData=>{
+router.get("api/workouts/range",async (req,res)=>{
+    try {
+        Workout.find()
+        .then((dbData)=>{
             res.json(dbData);
         })
-        .catch(err=>{
-            res.status(400).json(err);
-        })
+    }catch(err){
+        res.status(400).json(err);
+        console.log(err);
+    }
 })
 
 //Post a workout
-router.post("api/workouts",({body},res)=>{
-    Workout.create(body)
-        .then(dbData=>{
+router.post("api/workouts", async ({body},res)=>{
+    try{
+        Workout.create(body)
+        .then((dbData)=>{
             res.json(dbData);
         })
-        .catch(err=>{
-            res.status(404).json(err);
-        })
+    }catch(err){
+        res.status(404).json(err);
+    }
 })
+
+//Update workout
+router.put("api/workout/:id",async ({body,params},res)=>{
+    try{
+        Workout.findByIdAndUpdate(params.id,{$push:{exercise:body}})
+        .then((dbData)=>{
+            res.json(dbData);
+        })
+    }catch(err){
+            res.status(400).json(err);
+    }
+})
+
+module.exports = router;
