f0a1a222766dfb062699d4f97226b3a67387a320
JuneXll <71652307+JuneXll@users.noreply.github.com>
2021-07-20 14:25:46 -0400

    Completes server.js

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3000;
+
+//Creates Express app
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+//Creates mongoose db connection
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitnesstracker", {
+  useNewUrlParser: true,
+  useCreateIndex:true,
+  useFindAndModify: false
+});
+
+// routes
+app.use(require("./routes/api-routes.js"));
+app.use(require("./routes/html-routes"));
+
+//Listen request
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
