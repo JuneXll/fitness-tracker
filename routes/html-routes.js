02dbb9063f6f96199f20e73c307468644bd2fe16
JuneXll <71652307+JuneXll@users.noreply.github.com>
2021-07-20 15:42:27 -0400

    Fixed bug with css and linked style sheet correctly

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -2,15 +2,15 @@ const router = require("express").Router();
 const path = require ("path");
 
 router.get("/",(req,res)=>{
-    res.sendFile(path.join(__dirname + "/index.html"));
+    res.sendFile(path.join(__dirname + "../public/index.html"));
 });
 
 router.get("/exercise",(req,res)=>{
-    res.sendFile(path.join(__dirname + "../public/js/exercise.html"));
+    res.sendFile(path.join(__dirname + "../public/exercise.html"));
 });
 
 router.get("/stats",(req,res)=>{
-    res.sendFile(path.join(__dirname + "../stats/js/exercise.html"));
+    res.sendFile(path.join(__dirname + "../stats/exercise.html"));
 });
 
 module.exports = router;
